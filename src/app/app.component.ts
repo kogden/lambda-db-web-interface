9ff4e7d5fb9d1dae3cf6e00195a179c15eb814ff
Kevin Ogden <kevinogdendev@gmail.com>
2018-08-06 11:30:24 -0400

    basic setup, no service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  baseUrl = 'https://vivycsg88i.execute-api.us-east-1.amazonaws.com/dev/notes/';
+
+  constructor(private http: HttpClient) {}
+
+  getOne(value: string) {
+
+  }
+
+  getAll() {
+    return this.http.get(this.baseUrl);
+  }
 }
